2eabb2e8272156dd06d54b786bbf2a3127cec3c5
Emerson Cavalcanti <emerson.cavalcanti@escolatecnicalimoeiro.com.br>
2024-05-08 20:34:03 -0300

    interceptando hx-get com javascript

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-let nome = 'Jose'
-console.log(nome)
+document.addEventListener("htmx:afterRequest", function(event){
 
+if(event.target.id === 'busca-produtos'){
+    console.log(event.detail.xhr);
+
+    const response = JSON.parse(event.detail.xhr.responseText)
+    const produtosDiv = document.querySelector("#dados-produtos");
+
+    let htmlDiv = "";
+
+    response.products.forEach(produto => {
+        htmlDiv += `
+        <tr>
+         <td>${produto.id}</td>
+         <td>${produto.title}</td>
+         <td><a href="#"><i style="color:red; font-size:20px" class="bi bi-trash-fill"></i></a></td>
+        </tr>
+        `
+    })
+
+    produtosDiv.innerHTML = htmlDiv
+}
+
+});
