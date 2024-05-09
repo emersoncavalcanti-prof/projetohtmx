694108a33c04f1ed73b5aeb6acc7ae7e755a8612
Emerson Cavalcanti <emerson.cavalcanti@escolatecnicalimoeiro.com.br>
2024-05-08 21:23:17 -0300

    add preco na tabela

--- script.js
+++ script.js
@@ -9,13 +9,18 @@ if(event.target.id === 'busca-produtos'){
     let htmlDiv = "";
     let style = "";
     let indice = 0;
+    let preco = 0;
 
     response.products.forEach(produto => {
         style = indice%2==0 ? 'table-secondary' : ''
+        
+        preco = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(produto.price);
+        
         htmlDiv += `
         <tr class="${style}">
          <td>${produto.id}</td>
          <td>${produto.title}</td>
+         <td>${preco}</td>
          <td><a href="#"><i style="color:red; font-size:20px" class="bi bi-trash-fill"></i></a></td>
         </tr>
         `
