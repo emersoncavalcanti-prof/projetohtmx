21b01d2d810e47ab528ba38bcf54593e2ccb271a
Emerson Cavalcanti <emerson.cavalcanti@escolatecnicalimoeiro.com.br>
2024-05-15 21:24:14 -0300

    add imagens e um loading ao consultar api

--- script.js
+++ script.js
@@ -18,10 +18,11 @@ if(event.target.id === 'busca-produtos'){
         
         htmlDiv += `
         <tr class="${style}">
-         <td>${produto.id}</td>
-         <td>${produto.title}</td>
-         <td>${preco}</td>
-         <td><a href="#"><i style="color:red; font-size:20px" class="bi bi-trash-fill"></i></a></td>
+         <td class="col-1"><img src="${produto.thumbnail}" width="100" class="img-thumbnail"></td>
+         <td class="col-1">${produto.id}</td>
+         <td class="col-8">${produto.title}</td>
+         <td class="col-1">${preco}</td>
+         <td class="col-1"><a href="#"><i style="color:red; font-size:20px" class="bi bi-trash-fill"></i></a></td>
         </tr>
         `
         indice++
